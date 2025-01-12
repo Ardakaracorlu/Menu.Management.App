80538fbbb6600a9d046983526680b68753c51347
karacorlu98@gmail.com <karacorlu98@gmail.com>
2025-01-13 01:00:34 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,74 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using starterkit.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Http;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddScoped<StateService>();
+builder.Services.AddSingleton<AppState>();
+builder.Services.AddScoped<IActionService, ActionService>();
+builder.Services.AddScoped<MenuDataService>();
+builder.Services.AddWMBOS();
+builder.Services.AddScoped<NavScrollService>();
+builder.Services.AddSession();
+builder.Services.AddScoped<SessionService>();
+
+// Add session services
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30); // Adjust timeout as needed
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/login";
+        options.AccessDeniedPath = "/accessdenied";
+        options.Cookie.Name = "YourAppCookieName";
+        options.Cookie.HttpOnly = true;
+        options.Cookie.SameSite = SameSiteMode.Strict;
+        options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+    });
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseSession();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+    
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapFallbackToPage("/_Host");
+
+app.Run();
---
+++ Services/MenuData.Service.cs
@@ -0,0 +1,99 @@
+public class MenuDataService
+{
+    private List<MainMenuItems> MenuData = new List<MainMenuItems>()
+    {
+        new MainMenuItems(
+            menuTitle: "MAIN"
+        ),
+        new MainMenuItems(
+            type: "sub",
+            title: "Dashboards",
+            icon: "bx bx-home",
+            badgeValue: "12",
+            badgeClass: "bg-warning-transparent",
+            selected: false,
+            active: false,
+            dirChange: false,
+            children: new MainMenuItems[]
+            {
+              new MainMenuItems (
+                    path: "/home",
+                    type: "link",
+                    title: "CRM",
+                    selected: false,
+                    active: false,
+                    dirChange: false
+                ),
+            }
+        ),
+        new MainMenuItems (
+            type: "sub",
+            title: "Nested Menu",
+            icon: "bx bx-layer",
+            selected: false,
+            active: false,
+            dirChange: false,
+            children: new MainMenuItems[]
+            {
+                new MainMenuItems (
+                    path: "",
+                    type: "empty",
+                    title: "Nested-1",
+                    selected: false,
+                    active: false,
+                    dirChange: false
+                ),
+                new MainMenuItems (
+                    type: "sub",
+                    title: "Nested-2",
+                    selected: false,
+                    active: false,
+                    dirChange: false,
+                    children: new MainMenuItems[]
+                    {
+                        new MainMenuItems (
+                            path: "",
+                            type: "empty",
+                            title: "Nested-2-1",
+                            selected: false,
+                            active: false,
+                            dirChange: false
+                        ),
+                        new MainMenuItems (
+                            type: "sub",
+                            title: "Nested-2-2",
+                            selected: false,
+                            active: false,
+                            dirChange: false,
+                            children: new MainMenuItems[]
+                            {
+                                new MainMenuItems (
+                                    path: "",
+                                    type: "empty",
+                                    title: "Nested-2-2-1",
+                                    selected: false,
+                                    active: false,
+                                    dirChange: false
+                                ),
+                                new MainMenuItems (
+                                    path: "",
+                                    type: "empty",
+                                    title: "Nested-2-2-2",
+                                    selected: false,
+                                    active: false,
+                                    dirChange: false
+                                )
+                            }
+                        )
+                    }
+                )
+            }
+        ),
+        
+    };
+
+    public List<MainMenuItems> GetMenuData()
+   {
+        return MenuData;
+    }
+}
