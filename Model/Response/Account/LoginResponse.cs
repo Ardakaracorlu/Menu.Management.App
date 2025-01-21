56cfd36fc319bb6d1c421a3a08f68907c8f6ee00
karacorlu98@gmail.com <karacorlu98@gmail.com>
2025-01-21 13:55:11 +0300

    folder Changes

---
+++ Model/Response/Account/LoginResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Menu.Management.App.Model.Response.Account
+{
+    public class LoginResponse
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string AccessToken { get; set; }
+    }
+}
