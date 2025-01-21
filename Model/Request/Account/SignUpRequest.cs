92d89a63e8361755e8fce16ebdb06f3410aa6d34
karacorlu98@gmail.com <karacorlu98@gmail.com>
2025-01-21 20:00:29 +0300

    --

---
+++ Model/Request/Account/SignUpRequest.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using Menu.Management.App.Pages.Account;
+
+namespace Menu.Management.App.Model.Request.Account
+{
+    public class SignUpRequest
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Mail { get; set; }
+        public string Phone { get; set; }
+        public string Adress { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class SignUpValidator : AbstractValidator<SignUpRequest>
+    {
+        public SignUpValidator()
+        {
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Lütfen kullanıcı adınızı giriniz");
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Lütfen şifreinizi giriniz");
+        }
+    }
+}
